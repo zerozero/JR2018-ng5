6ddd698faf0b414aa3e67ac1a7686722c0f77c11
Jon Rowe <mail@jonrowe.com>
2018-03-06 19:38:31 +0000

    add express server for heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jonrowe on 06/03/2018.
+ */
+//Install express server
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
