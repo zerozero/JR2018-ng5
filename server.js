0dbedec0daf308a52f0995e6da79db9d50836a04
Jon Rowe <mail@jonrowe.com>
2018-03-07 09:13:41 +0000

    modify server.js in attempt to fix cannot GET / problem

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
